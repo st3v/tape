c64047e5a9d31b8c7aaa235d7976ae70fede13ca
st3v <stev@kiva.org>
2013-12-19 17:25:26 -0800

    No more tabs

--- vagrant/puppet/modules/elasticsearch/manifests/init.pp
+++ vagrant/puppet/modules/elasticsearch/manifests/init.pp
@@ -1,17 +1,17 @@
 class elasticsearch ($version) {
 
-	exec{'get-es-package':
-	  	command => "/usr/bin/wget -q https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-${version}.deb -O /tmp/elasticsearch-${version}.deb",
-	  	creates => "/tmp/elasticsearch-${version}.deb",
-		unless => "/usr/bin/test -f /usr/share/elasticsearch/bin/elasticsearch && /usr/share/elasticsearch/bin/elasticsearch -v | grep -ce 'Version: ${version},.*'",
-  		timeout  => 1800
-	}
+    exec{'get-es-package':
+        command => "/usr/bin/wget -q https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-${version}.deb -O /tmp/elasticsearch-${version}.deb",
+        creates => "/tmp/elasticsearch-${version}.deb",
+        unless => "/usr/bin/test -f /usr/share/elasticsearch/bin/elasticsearch && /usr/share/elasticsearch/bin/elasticsearch -v | grep -ce 'Version: ${version},.*'",
+        timeout  => 1800
+    }
 
-	package {'elasticsearch':
-		provider => dpkg,
-		ensure => latest,
+    package {'elasticsearch':
+        provider => dpkg,
+        ensure => latest,
         source => "/tmp/elasticsearch-${version}.deb",
-		require => Exec['get-es-package']
-	}
+        require => Exec['get-es-package']
+    }
 
 }
--- vagrant/puppet/modules/typesafe/manifests/init.pp
+++ vagrant/puppet/modules/typesafe/manifests/init.pp
@@ -1,48 +1,48 @@
 
 class typesafe ($version) {
 
-	exec{'get-activator':
-	  	command => "/usr/bin/wget -q http://downloads.typesafe.com/typesafe-activator/${version}/typesafe-activator-${version}.zip -O /tmp/typesafe-activator-${version}.zip",
-	  	creates  => "/tmp/typesafe-activator-${version}.zip",
-  		timeout  => 1800
-	}
+    exec{'get-activator':
+        command => "/usr/bin/wget -q http://downloads.typesafe.com/typesafe-activator/${version}/typesafe-activator-${version}.zip -O /tmp/typesafe-activator-${version}.zip",
+        creates  => "/tmp/typesafe-activator-${version}.zip",
+        timeout  => 1800
+    }
 
-	package {'unzip':
-		provider => apt,
-		ensure => present
-	}
+    package {'unzip':
+        provider => apt,
+        ensure => present
+    }
 
-	exec{'unzip-activator':
-		command => "/usr/bin/sudo /usr/bin/unzip /tmp/typesafe-activator-${version}.zip -d /usr/share/typesafe",
-	  	require => [Package['unzip'], Exec['get-activator']],
-	  	unless  => "/usr/bin/test -d /usr/share/typesafe/activator-${version}"
-	}
+    exec{'unzip-activator':
+        command => "/usr/bin/sudo /usr/bin/unzip /tmp/typesafe-activator-${version}.zip -d /usr/share/typesafe",
+        require => [Package['unzip'], Exec['get-activator']],
+        unless  => "/usr/bin/test -d /usr/share/typesafe/activator-${version}"
+    }
 
-	exec{'unlink-activator':
-		command => "/usr/bin/sudo /bin/rm -f /usr/local/bin/activator",
-		require => Exec['unzip-activator'],
-		onlyif => "/usr/bin/test -h /usr/local/bin/activator"
-	}
+    exec{'unlink-activator':
+        command => "/usr/bin/sudo /bin/rm -f /usr/local/bin/activator",
+        require => Exec['unzip-activator'],
+        onlyif => "/usr/bin/test -h /usr/local/bin/activator"
+    }
 
-	exec{'link-activator':
-		command => "/usr/bin/sudo /bin/ln -s /usr/share/typesafe/activator-${version}/activator /usr/local/bin/",
-		require => Exec['unlink-activator']
-	}
+    exec{'link-activator':
+        command => "/usr/bin/sudo /bin/ln -s /usr/share/typesafe/activator-${version}/activator /usr/local/bin/",
+        require => Exec['unlink-activator']
+    }
 
-	exec{'chmod-activator':
-		command => "/usr/bin/sudo /bin/chmod 755 /usr/local/bin/activator",
-		require => Exec['link-activator'],
-		onlyif => "/usr/bin/test -f /usr/local/bin/activator"
-	}
+    exec{'chmod-activator':
+        command => "/usr/bin/sudo /bin/chmod 755 /usr/local/bin/activator",
+        require => Exec['link-activator'],
+        onlyif => "/usr/bin/test -f /usr/local/bin/activator"
+    }
 
-	file {'/home/vagrant/.activator':
-	    ensure => "directory",
-	    owner  => "vagrant",
-	    group  => "vagrant",
-	    mode   => 750,
-	}
+    file {'/home/vagrant/.activator':
+        ensure => "directory",
+        owner  => "vagrant",
+        group  => "vagrant",
+        mode   => 750,
+    }
 
-	file { "/home/vagrant/.activator/activatorconfig.txt":
+    file { "/home/vagrant/.activator/activatorconfig.txt":
         mode => "0644",
         owner => 'vagrant',
         group => 'vagrant',
@@ -50,9 +50,9 @@ class typesafe ($version) {
         require => File['/home/vagrant/.activator']
     }
 
-	#exec{'run-activator':
-	#	command => "/usr/local/bin/activator ui >> /var/log/activator.log 2>&1 &",
-	#	require => Exec['link-activator']
-	#}
+    #exec{'run-activator':
+    #   command => "/usr/local/bin/activator ui >> /var/log/activator.log 2>&1 &",
+    #   require => Exec['link-activator']
+    #}
 
 }
